f5ded7a763bc2c89d471f3a5d5ded3a609a6efa6
Puck Wang <zwang684@uottawa.ca>
2022-02-24 13:43:02 -0500

    added gnerality to silhouette score function

--- TF-IDF/measures.py
+++ TF-IDF/measures.py
@@ -2,10 +2,9 @@ from typing import List
 from matplotlib.pyplot import cohere
 import pandas as pd
 import numpy as np
-from gensim import models
+from gensim.models import LsiModel
 from gensim.models.coherencemodel import CoherenceModel
-from sklearn.cluster import KMeans
-from sklearn.metrics import cohen_kappa_score, adjusted_rand_score, rand_score, silhouette_score
+from sklearn.metrics import cohen_kappa_score, adjusted_rand_score, silhouette_score
 
 """
 calculate the best amount of topics for LSI model
@@ -15,7 +14,7 @@ def best_num_topic(corpus, dictionary, max_n_topic)->List:
     results = []
 
     for t in range(2, max_n_topic):
-        lsi_model = models.LsiModel(corpus, id2word=dictionary, num_topics=t)
+        lsi_model = LsiModel(corpus, id2word=dictionary, num_topics=t)
         corpus_lsi = lsi_model[corpus]
 
         cm = CoherenceModel(model=lsi_model, corpus=corpus_lsi, coherence='u_mass')
@@ -33,21 +32,23 @@ def best_num_topic(corpus, dictionary, max_n_topic)->List:
 """
 calculate the best k for kmean based on silhouette score
 """
-def best_silhouette_score(corpus, dictionary, max_n_topic):
+def best_silhouette_score(model, corpus, dictionary, max_n_clusters, best_num_topic = 2):
     from sklearn.metrics import silhouette_score
     np.random.seed(0)
 
     results = []
+    lsi_model = LsiModel(corpus, id2word=dictionary, num_topics=best_num_topic)
+    corpus_lsi = lsi_model[corpus]
 
-    for t in range(2, max_n_topic):
+    for t in range(2, max_n_clusters):
  
-        lsi_model = models.LsiModel(corpus, id2word=dictionary, num_topics=7)
-        corpus_lsi = lsi_model[corpus]
-        
         X = np.array([[tup[1] for tup in arr] for arr in corpus_lsi])
-        kmeans = KMeans(n_clusters=t, random_state=0).fit(X)
+        if 'random_state' in model.get_params():
+            model.set_params(random_state = 0)
+        
+        models = model.set_params(n_clusters = t).fit(X)
 
-        score = silhouette_score(X, kmeans.labels_)
+        score = silhouette_score(X, models.labels_)
 
         tup = t, score
         results.append(tup)
